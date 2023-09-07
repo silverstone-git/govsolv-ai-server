aef74b8d41eee641fef4590f29cba87c5278e976
silverstone-git <silverstone965@gmail.com>
2023-09-07 22:18:00 +0530

    made a message handling function; made the response more api-ish having status codes;

--- myapp/views.py
+++ myapp/views.py
@@ -9,16 +9,35 @@ import json
 def index(request):
     return HttpResponse("Hemlo domst")
 
+
+def spam_check(message):
+    # take in a message, check spam, send Boolean and Integer in a list
+    # denoting the tolerance checked value for if message is indeed spam or not
+    # and the integer stands for the probability in percentage
+    return [True, 60]
+
+
 @csrf_exempt
 def spam(request):
     access = ""
+    message = ""
     try:
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
         access = body['access']
+        message = body['message']
     except Exception as error:
         print(error)
-    if access and access == settings.ACCESS_TOKEN:
-        return HttpResponse("{spam:true, probability:70}")
+    spam_res = spam_check(message)
+    spam_string = "true"
+    if spam_res[0]:
+        spam_string = "true"
     else:
-        return HttpResponse("404")
+        spam_string = "false"
+    spam_probability = str(spam_res[1])
+    if access and message and access == settings.ACCESS_TOKEN:
+        return HttpResponse("{spam:" + spam_string + ", probability:" + spam_probability + "}", status = 200)
+    elif access != settings.ACCESS_TOKEN:
+        return HttpResponse("404", status = 404)
+    else:
+        return HttpResponse("400", status = 400)
