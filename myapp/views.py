814de18d149a349c1fe2e3294320cde9155d39ff
silverstone-git <silverstone965@gmail.com>
2023-09-08 20:10:46 +0530

    added random to test the frontend out for all outcomes; put the data into a wrapper;

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 import json
+from random import random
 
 # Create your views here.
 
@@ -14,7 +15,12 @@ def spam_check(message):
     # take in a message, check spam, send Boolean and Integer in a list
     # denoting the tolerance checked value for if message is indeed spam or not
     # and the integer stands for the probability in percentage
-    return [True, 60]
+    probability = int(random() * 100)
+    probRand = random()
+    isSpam = True
+    if probRand > 0.5:
+        isSpam = False
+    return [isSpam, probability]
 
 
 @csrf_exempt
@@ -36,8 +42,8 @@ def spam(request):
         spam_string = "false"
     spam_probability = str(spam_res[1])
     if access and message and access == settings.ACCESS_TOKEN:
-        return HttpResponse("{spam:" + spam_string + ", probability:" + spam_probability + "}", status = 200)
+        return HttpResponse("{'success': true, 'data': {spam: " + spam_string + ", probability: " + spam_probability + "}, 'message': 'Successfully evaluated'}", status = 200)
     elif access != settings.ACCESS_TOKEN:
-        return HttpResponse("404", status = 404)
+        return HttpResponse("{'success': false, 'data': {}, 'message': 'Wrong Access Token'}", status = 404)
     else:
-        return HttpResponse("400", status = 400)
+        return HttpResponse("{'success': false, 'data': {}, 'message': 'Missing some properties in request, did you give message and access in the request body?'}", status = 400)
